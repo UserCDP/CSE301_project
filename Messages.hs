ac6ad56c30d25091874bade42a40b853b1522a63
Gabrio05 <73126191+Gabrio05@users.noreply.github.com>
2024-10-31 15:08:03 +0100

    Update Messages.hs
    Changed some messages, but this is kind of a mess, at first I thought it was worse than it was because I didn't get it was a dictionary lol, but at least it is one.

--- Messages.hs
+++ Messages.hs
@@ -8,7 +8,7 @@ import qualified Data.Map.Strict as Map
 
 messagesDict :: Map.Map String String
 messagesDict = 
-    Map.insert "welcome" "OH NOOO! You have been thrown into a dangerous mine. Oxigen is running low, find a way to escape! \n Type h for help" $
+    Map.insert "welcome" "Oh no! You find yourself in a mine and the air is getting thin. Find a way to open the trap and escape before you suffocate! \n Type h for help." $
     Map.insert "blocked way" "The way is blocked by debris" $
     Map.insert "out of bounds" "Cannot descend any further." $
     Map.insert "blocked entrance" "Find another way " $
@@ -22,19 +22,18 @@ messagesDict =
     Map.insert "empty chest" "The chest is empty" $
     Map.insert "locked chest" "Chest is locked" $
     Map.insert "dont have key" "You don't have that key " $
-    Map.insert  "no key here" "There is no key to pick up here " $
+    Map.insert "no key here" "There is no key to pick up here " $
     Map.insert "clear way" "Debris removed! " $
     Map.insert  "shovel is useless" "The way is clear, no need for a shovel ..." $
     Map.insert "no shovel" "You dont have any shovels " $
     Map.insert "quit" "Quiting already?\n Here's the progress:" $
     Map.insert "parseError" "Sorry, I don't understand" $
-    Map.insert "helpString" "This is a text adventure game" $
+    Map.insert "helpString" "Use 'go' to move down, left or right such as 'go left' or 'go down'." $
     Map.insert "Debris" "The way is blocked by debris. Use a Shovel ..." $
     Map.insert "Key" "There's a key here, it might be useful ..." $
     Map.insert "Locked Chest" "There is a locked chest here. Go find a key to open it ..." $
     Map.insert "Unlocked Full Chest" "There's an open chest here. Go check it ..." $
     Map.insert "Unlocked Empty Chest" "There's an empty chest here ..." $
-    Map.insert "helpString" "This is a text adventure game" $
     Map.insert "initialVisit" "" $
     Map.insert "familiarPlace" "This place seems familiar ... " $
     Map.insert "lost" "You're definitely lost. " $
@@ -44,9 +43,9 @@ messagesDict =
     Map.insert "empty inventory" "Your inventory is currently empty " $
     Map.insert "inventory contains" "Your inventory contains: "$
     Map.insert "suffocated" "\n You suffocated to death :(" $
-    Map.insert "oxigenLeft" "Oxigen left: " $
-    Map.insert "used oxigen tank" "Oxigen tank consumed" $
-    Map.insert "dont have oxigen tank" "You dont have that oxigen tank" $
+    Map.insert "oxygenLeft" "Oxygen left: " $
+    Map.insert "used oxygen tank" "Oxygen tank consumed" $
+    Map.insert "dont have oxygen tank" "You dont have that oxygen tank" $
     Map.insert "pula" "pizda" Map.empty
     
 
@@ -60,5 +59,3 @@ displayString str = putStrLn $ getString str
 
 displayStringWithVal :: Show a => String -> a -> IO ()
 displayStringWithVal str x = putStrLn $ getString str ++ show x
-
-
