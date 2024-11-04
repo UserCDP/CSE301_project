958b999dd04e0688071b9a12984551296c60124b
Gabrio05 <73126191+Gabrio05@users.noreply.github.com>
2024-11-04 18:28:51 +0100

    Update a bunch of messages to be better.
    Add full stops and better descrip

--- Messages.hs
+++ Messages.hs
@@ -8,45 +8,44 @@ import qualified Data.Map.Strict as Map
 
 messagesDict :: Map.Map String String
 messagesDict = 
-    Map.insert "welcome" "Oh no! You find yourself in a mine and the air is getting thin. Find a way to open the trap and escape before you suffocate! \n Type h for help." $
-    Map.insert "blocked way" "The way is blocked by debris" $
+    Map.insert "welcome" "Oh no! You find yourself in a mine and the air is getting thin. Find a way to open the trap door above (it has a giant key hole) and escape before you suffocate! \n Type help for help." $
+    Map.insert "blocked way" "The way is blocked by debris." $
     Map.insert "out of bounds" "Cannot descend any further." $
-    Map.insert "blocked entrance" "Find another way " $
-    Map.insert "new key" "You picked up a new key " $
-    Map.insert "unlocked chest" "Chest unlocked " $
-    Map.insert "wrong key" "Key doesn't work, perhaps try another one " $
-    Map.insert "chest already open""The chest is already open " $
-    Map.insert "nothing to open" "There's nothing to open " $
-    Map.insert "emptying chest" "Picked up chest content" $
-    Map.insert "no chest here" "There's no chest here" $
-    Map.insert "empty chest" "The chest is empty" $
-    Map.insert "locked chest" "Chest is locked" $
-    Map.insert "dont have key" "You don't have that key " $
-    Map.insert "no key here" "There is no key to pick up here " $
-    Map.insert "clear way" "Debris removed! " $
-    Map.insert  "shovel is useless" "The way is clear, no need for a shovel ..." $
-    Map.insert "no shovel" "You dont have any shovels " $
+    Map.insert "blocked entrance" "Find another way." $
+    Map.insert "new key" "You picked up a new key." $
+    Map.insert "unlocked chest" "Chest unlocked, don't forget to take its contents!" $
+    Map.insert "wrong key" "Key doesn't work, perhaps try another one." $
+    Map.insert "chest already open""The chest is already open." $
+    Map.insert "nothing to open" "There's nothing to open." $
+    Map.insert "emptying chest" "Picked up chest content. Check inventory for the new items. Type i or inventory for your inventory." $
+    Map.insert "no chest here" "There's no chest here." $
+    Map.insert "empty chest" "The chest is empty." $
+    Map.insert "locked chest" "Chest is locked." $
+    Map.insert "dont have key" "You don't have that key." $
+    Map.insert "no key here" "There is no key to pick up here." $
+    Map.insert "clear way" "Debris removed!" $
+    Map.insert  "shovel is useless" "The way is clear, no need for a shovel." $
+    Map.insert "no shovel" "You don't have any shovels." $
     Map.insert "quit" "Quiting already?\n Here's the progress:" $
-    Map.insert "parseError" "Sorry, I don't understand" $
-    Map.insert "helpString" "Use 'go' to move up, left or right such as 'go left' or 'go up'." $
-    Map.insert "Debris" "The way is blocked by debris. Use a Shovel ..." $
-    Map.insert "Key" "There's a key here, it might be useful ..." $
-    Map.insert "Locked Chest" "There is a locked chest here. Go find a key to open it ..." $
-    Map.insert "Unlocked Full Chest" "There's an open chest here. Go check it ..." $
-    Map.insert "Unlocked Empty Chest" "There's an empty chest here ..." $
-    Map.insert "initialVisit" "" $
-    Map.insert "familiarPlace" "This place seems familiar ... " $
-    Map.insert "lost" "You're definitely lost. " $
-    Map.insert "" "Explore further" $
-    Map.insert "no objects" "Nothing of use here ... "  $
-    Map.insert "dont have object" "You dont have that object" $
-    Map.insert "empty inventory" "Your inventory is currently empty " $
+    Map.insert "parseError" "Sorry, please try again. Use h or help for some help." $
+    Map.insert "helpString" "Type 'move', 'pick up', 'use', 'inventory', or 'oxygen' for the various commands. If their use is incorrect, you will be told how to use them." $
+    Map.insert "Debris" "The way is blocked by debris, use a shovel." $
+    Map.insert "Key" "There's a key here, it might be useful." $
+    Map.insert "Locked Chest" "There is a locked chest here with a key hole." $
+    Map.insert "Unlocked Full Chest" "There's an open chest here with items." $
+    Map.insert "Unlocked Empty Chest" "There's an empty chest here." $
+    Map.insert "initialVisit" "This seems new or kind of new, like you're visiting it for the first time or first couple of times." $
+    Map.insert "familiarPlace" "This place seems familiar, like you've already visited it." $
+    Map.insert "lost" "You've been here already, you're certain." $
+    Map.insert "" "" $
+    Map.insert "no objects" "Nothing of use here."  $
+    Map.insert "dont have object" "You don't have that object." $
+    Map.insert "empty inventory" "Your inventory is currently empty." $
     Map.insert "inventory contains" "Your inventory contains: "$
-    Map.insert "suffocated" "\n You suffocated to death :(" $
+    Map.insert "suffocated" "\n You suffocated to death, UwU :P International Nuclear and Radiological Event Scale you silly goose" $
     Map.insert "oxygenLeft" "Oxygen left: " $
-    Map.insert "used oxygen tank" "Oxygen tank consumed" $
-    Map.insert "dont have oxygen tank" "You dont have that oxygen tank" $
-    Map.insert "pula" "pizda" Map.empty
+    Map.insert "used oxygen tank" "Oxygen tank consumed. Check your oxygen (even though it's air, not pure oxygen, you'd die otherwise,\nI mean seriously, who would say 'oxygen', you breathe air, not oxygen, nobody breathes oxygen,/nwell that's not true, and they don't even actually have oxygen tanks in mines, how did you even find one? Was it just lying around?/nThat's stupidly irresponsible of whoever put that oxygen tank there, it's a fire hazard after all.)." $
+    Map.insert "dont have oxygen tank" "You dont have an oxygen tank." $ Map.empty
     
 
 getString :: String -> String
