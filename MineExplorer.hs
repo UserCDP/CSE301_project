e566a9dcd7ec0b7d4d4da4ab9b82006ef1261a85
Daniela Cojocaru <62444350+UserCDP@users.noreply.github.com>
2024-11-04 20:03:54 +0100

    added the code for the background music
    i wanted to add the music file too, but it was too large

--- MineExplorer.hs
+++ MineExplorer.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module MineExplorer where
 
 import GameData
@@ -9,11 +10,17 @@ import System.IO
 import Control.Concurrent.Async (race)
 import System.Timeout (timeout)
 
+import SDL
+import SDL.Mixer as Mixer
+import Control.Monad (unless)
 
 --- The game loop.     
 
 go :: Game -> IO (Maybe Game)
 go game1 = do
+  SDL.initialize [SDL.InitAudio]
+  Mixer.openAudio Mixer.defaultAudio 2048
+  Mixer.loadMP3 "soundtrack.mp3" >>= Mixer.playMusic Mixer.Forever
   showCurrentNode game1
   let game = game1 {newPos = False}
   putStr "> "                                      
@@ -24,6 +31,15 @@ go game1 = do
     Right line -> do
       newGame <- executeCommand (parseInput parseCmd line) game
       if (gameOver newGame) then return Nothing else go newGame
+
+-- Add the music to the game
+sinSamples :: [Int16]
+sinSamples =
+  map (\n ->
+         let t = fromIntegral n / 48000 :: Double
+             freq = 440
+         in round (fromIntegral (maxBound `div` 2 :: Int16) * sin (2 * pi * freq * t)))
+      [0 :: Int32 ..]
   
 
 main :: IO (Maybe Game)
@@ -31,3 +47,4 @@ main = do
   displayString "welcome"
   initialGameState <- resetGame a_tree
   go initialGameState
+   
