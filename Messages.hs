3489b35ef293310a64cc4579435eff77e972520a
Gabrio05 <73126191+Gabrio05@users.noreply.github.com>
2024-11-08 17:21:33 +0100

    Update Messages.hs
    It's not a mine anymore but a crypt

--- Messages.hs
+++ Messages.hs
@@ -8,7 +8,7 @@ import qualified Data.Map.Strict as Map
 
 messagesDict :: Map.Map String String
 messagesDict = 
-    Map.insert "welcome" "Oh no! You find yourself in a mine and the air is getting thin. Find a way to open the trap door above (it has a giant key hole) and escape before you suffocate! \n Type help for help. " $
+    Map.insert "welcome" "Oh no! Someone locked you up in the tree-like catacombs. \nFind a way to open the trap door above with a key and escape before you go mad! \nType help for help. " $
     Map.insert "blocked way" "The way is blocked by debris. " $
     Map.insert "out of bounds" "Cannot descend through here. " $
     Map.insert "hit a wall" "There's a wall here, try another direction. " $
@@ -30,7 +30,7 @@ messagesDict =
     Map.insert "no shovel" "You don't have any shovels. " $
     Map.insert "quit" "Quiting already? \nHere's the progress: " $
     Map.insert "parseError" "Sorry, please try again. Use h or help for some help. " $
-    Map.insert "helpString" "Type 'move', 'get', 'use', 'inventory', or 'oxygen' for the various commands. \nThe command 'move' is followed by 'left', 'right', 'down', or 'up'. \nThe command 'get' is followed by 'item' in order to pick up any item at the location. \n'use' requires either the item followed by an ID (such as a key ID) or simply the item to use it. \n'inventory' displays the inventory and, most impotantly, 'oxygen' displays the remaining units of seconds. " $
+    Map.insert "helpString" "Type 'move', 'get', 'use', 'inventory', or 'sanity' for the various commands. \nThe command 'move' is followed by 'left', 'right', 'down', or 'up'. \nThe command 'get' is followed by 'item' in order to pick up any item at the location. \n'use' requires either the item followed by an ID (such as a key ID) or simply the item to use it. \n'inventory' displays the inventory and, most impotantly, 'sanity' displays the remaining units of sanity until you go mad. " $
     Map.insert "Debris" "The way is blocked by debris, use a shovel. " $
     Map.insert "Key" "There's a key here, it might be useful. " $
     Map.insert "Locked Chest" "There is a locked chest here with a key hole. " $
@@ -44,9 +44,9 @@ messagesDict =
     Map.insert "dont have object" "You don't have that object. " $
     Map.insert "empty inventory" "Your inventory is currently empty. " $
     Map.insert "inventory contains" "Your inventory contains: "$
-    Map.insert "suffocated" "\n You have met your demise exploring the mine. Cause of death: Lack of breathable air. " $
-    Map.insert "oxygenLeft" "Oxygen left: " $
-    Map.insert "used oxygen tank" "Oxygen tank consumed. Check your oxygen (even though it's air, not pure oxygen, you'd die otherwise,\nI mean seriously, who would say 'oxygen', you breathe air, not oxygen, nobody breathes oxygen,\nwell that's not true, and they don't even actually have oxygen tanks in mines, how did you even find one? Was it just lying around?\nThat's stupidly irresponsible of whoever put that oxygen tank there, it's a fire hazard after all). " $
+    Map.insert "suffocated" "\nYou have gone mad in the crypt! You couldn't exit in time. " $
+    Map.insert "oxygenLeft" "Sanity left: " $
+    Map.insert "used oxygen tank" "Oxygen tank consumed. Your sanity has increased with a breath of fresh oxygen (even though it's air, not pure oxygen, you'd die otherwise,\nI mean seriously, who would say 'oxygen', you breathe air, not oxygen, nobody breathes oxygen,\nwell that's not true, and they don't even actually have oxygen tanks in crypts, how did you even find one? Was it just lying around?\nThat's stupidly irresponsible of whoever put that oxygen tank there, it's a fire hazard after all). " $
     Map.insert "dont have oxygen tank" "You don't have an oxygen tank. " $ Map.empty
 
     
